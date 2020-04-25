29e4aec92ccffad53ac36d838251d72f647a5e94
myk245 <myk245@nyu.edu>
2020-04-24 23:53:42 -0400

    seed data for disorders and symptoms

---
+++ app/controllers/users_controller 2.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+   def index
+      users = User.all 
+      render json: users, except: [:created_at, :updated_at]
+   end
+
+   def show
+      user = User.find(params[:id])
+      render json: user, except: [:created_at, :updated_at]
+   end 
+end
