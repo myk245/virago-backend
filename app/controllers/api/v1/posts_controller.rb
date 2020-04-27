97ae6fb5047cce1980c26bbf366a6abbd8a09112
myk245 <myk245@nyu.edu>
2020-04-27 17:02:47 -0400

    include comments association in PostsController

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PostsController < ApplicationController
    def index 
       posts = Post.all
-      render json: posts, except: [:created_at, :updated_at], include: [:user, :comment]
+      render json: posts, except: [:created_at, :updated_at], include: [:user, :comments]
    end
 
    def show
