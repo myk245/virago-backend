f594dbfa2e7b8680a0be4d4247bd21b0c15e7c62
myk245 <myk245@nyu.edu>
2020-04-28 02:12:35 -0400

    associations in show action in PostsController

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::PostsController < ApplicationController
    def index 
       posts = Post.all
-      render json: posts, except: [:created_at, :updated_at], include: [:comments, :user => {except: :password}]
+      render json: posts, except: [:created_at, :updated_at], include: [:comments => {include: :user}, :user => {except: :password}]
+      # Post.first.comments.first.user.username
    end
 
    def show
       post = Post.find(params[:id]) 
-      render json: post, except: [:created_at, :updated_at]
+      render json: post, except: [:created_at, :updated_at], include: [:comments => {include: :user}, :user => {except: :password}]
    end 
 
    def create
